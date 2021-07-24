aa369caf259379377771cbd2d7a69868d1a8abb9
Abdullah Al Bulushi <82784558+HitmanOM@users.noreply.github.com>
2021-07-24 09:21:45 +0400

    Update base-64.py

--- base-64.py
+++ base-64.py
@@ -11,22 +11,22 @@ print('''
                                                            By HitmanOM''')
 while True:
 
-    print('1 : Encode')
-    print('2 : Decode')
-    in1 = input('Enter Your Choice: ') 
+    print('[$] 1 : Encode')
+    print('[$] 2 : Decode')
+    in1 = input('[$] Enter Your Choice: ') 
     if in1 in ('1','2'):
         if in1 == '1':
-            text = input('Enter The Text you Want To encode: ')
+            text = input('[$] Enter The Text you Want To encode: ')
             encoded_str = base64.b64encode(text.encode())
-            print('encoded text:>('' ',(encoded_str),')')    
+            print('[$] encoded text:>('' ',(encoded_str),')')    
         elif in1 == '2': 
-            text2 = input('Enter The Text you Want To decode: ')                   
-            print('decoded base64:>',base64.b64decode(text2).decode())
+            text2 = input('[$] Enter The Text you Want To decode: ')                   
+            print('[$] decoded base64:>',base64.b64decode(text2).decode())
         else:
-            print('error')
+            print('[$] error')
     else:
-        print('enter a valid choice')
-    inp2 = input('En/Decode Again ? (y or n) : ')
+        print('[$] enter a valid choice')
+    inp2 = input('[$] En/Decode Again ? (y or n) : ')
     if inp2 == 'n':
         break
             #by HitmanOM
